f3ab3223f7cadd672034392357c125f8993eb10e
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-12 10:30:55 -0600

    add token fixture, fix date on story and comments, loaddata to db

--- colorstory-server/colorstory/colorstoryapi/models/story.py
+++ colorstory-server/colorstory/colorstoryapi/models/story.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Story(models.Model):
-    color = models.CharField()
-    word_prompt = models.CharField()
+    color = models.CharField(max_length=10)
+    word_prompt = models.CharField(max_length=30)
     private = models.BooleanField()
     content = models.TextField(max_length=2000)
     title = models.CharField(max_length=30)
